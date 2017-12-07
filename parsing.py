97cd6e327cdf8b361de07182e8829cd78c3c9ccb
Mario Figueiró Zemor <mario.figueiro@ufrgs.br>
2017-12-07 21:58:24 -0200

    Add files via upload

--- parsing.py
+++ parsing.py
@@ -1,4 +1,5 @@
 from classes.Estudante import *;
+from classes.ProvaObjetiva import *;
 import classes.Database as db;
 import classes.file as csvfile;
 import logging
@@ -6,18 +7,66 @@ import config.logger as log;
 
 logger = logging.getLogger(__name__);
 
+MAX_TO_COMMIT=5
+
 def insertEstudents(data,dd):
     conn = db.connect();
     cursor = conn.cursor();
     i = 0;
     while True:
         try:
-            columns = csvfile.readLine(data);
+            line = csvfile.readLine(data);
             values = Estudante.getValues(columns,dd);
             Estudante.insert(values,cursor);
             i = i +1;
-            if i%50000 ==0:
+            if i%MAX_TO_COMMIT ==0:
                 print("commit: total students: {}".format(i));
+                conn.commit();
+        except Exception as e:
+            logger.error("Exception during inserting students: {}.".format(e));
+            print("error: {}".format(e));          
+            conn.commit();
+            break;
+
+def inserirProvaObjetiva(data,dd):
+    conn = db.connect();
+    cursor = conn.cursor();
+    i = 0;
+    while True:
+        try:
+            columns = csvfile.readLine(data);
+            valuesCN = ProvaObjetiva.getValuesCN(columns,dd);
+            valuesCH = ProvaObjetiva.getValuesCH(columns,dd);
+            valuesLC = ProvaObjetiva.getValuesLC(columns,dd);
+            valuesMT = ProvaObjetiva.getValuesMT(columns,dd);
+
+            ProvaObjetiva.insert(valuesCN,cursor);
+            ProvaObjetiva.insert(valuesCH,cursor);
+            ProvaObjetiva.insert(valuesLC,cursor);
+            ProvaObjetiva.insert(valuesMT,cursor);
+            i = i+1;
+            if i%MAX_TO_COMMIT ==0:
+                print("commit: total ProvaObjetiva: {}".format(i));
+                conn.commit();
+                break;
+        except Exception as e:
+            logger.error("Exception during inserting objective tests: {}.".format(e));
+            print("erro during inseirProvaObjetiva: {}".format(e));
+            conn.commit();
+            break;
+
+def inserirProvaRedacao(data,dd):
+    conn = db.connect();
+    cursor = conn.cursor();
+    i = 0;
+    while True:
+        try:
+            columns = csvfile.readLine(data);
+            values = ProvaRedacao.getValues(columns,dd);
+            ProvaRedacao.insert(values,cursor);
+            i = i +1;
+            if i%MAX_TO_COMMIT ==0:
+                print("commit: total ProvaRedacao: {}".format(i));
                 conn.commit();
         except Exception as e:
             logger.error("Exception during inserting students: {}.".format(e));
@@ -32,5 +81,6 @@ if __name__ == "__main__":
     open_mode = "rb";
     data = csvfile.openFile(microdados,open_mode);
     data_dict = csvfile.createDict(data);
-    insertEstudents(data,data_dict);
+    #insertEstudents(data,data_dict);
+    inserirProvaObjetiva(data,data_dict);
     print("done!");
