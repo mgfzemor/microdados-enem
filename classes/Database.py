c8c4833219643477c0141f9d228d1e06b09e9c87
mgfzemor <mario.ufrgs.inf@gmail.com>
2017-12-27 00:17:04 -0200

    [fix] - Convert Database into class with static methods.

--- classes/Database.py
+++ classes/Database.py
@@ -7,12 +7,26 @@ import logging
 
 logger = logging.getLogger(__name__);
 
-def connect():
-    ret = None;
-    try:
-        logger.debug("connecting to postgres db: {} {} {}".format(db.host,db.user,db.dbname));
-        conn = psycopg2.connect(host=db.host,user=db.user,password=db.password,dbname=db.dbname);
-        ret = conn;
-    except Exception as e:
-        logger.error("Exception during postgres connetion: {}".format(e));
-    return ret;
+class Database():
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def connect():
+        ret = None;
+        try:
+            logger.debug("connecting to postgres db: {} {} {}".format(db.host,db.user,db.dbname));
+            conn = psycopg2.connect(host=db.host,user=db.user,password=db.password,dbname=db.dbname);
+            ret = conn;
+        except Exception as e:
+            logger.error("Exception during postgres connetion: {}".format(e));
+        return ret;
+
+    @staticmethod
+    def getCursor():
+        try:
+            connection = Database.connect();
+            cursor = connection.cursor();
+        except Exception as e:
+            logger.error("class: (Database) method: (getCursor) - Exception during get cursor: {}".format(e));
+        return cursor;
